f20eec59aa1b9bb9fd78b4e67f2c28b2ee7db615
Petr Karmashev <smonkl@bk.ru>
2016-07-28 23:12:33 +0300

    ShutdownHandler as shutdown hook in Launcher

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ import battle.Launcher;
  */
 public class Main
 {
-    public static void main(String[] args)
+
+    public static void main(String[] args) throws Exception
     {
         new Launcher().run();
     }
---
+++ src/main/java/battle/handler/ShutdownHandler.java
@@ -0,0 +1,24 @@
+package battle.handler;
+
+import com.sun.net.httpserver.HttpServer;
+
+/**
+ * Stops HttpServer
+ * @author Vehsamrak
+ */
+public class ShutdownHandler implements Runnable
+{
+
+    private HttpServer server;
+
+    public ShutdownHandler(HttpServer server)
+    {
+        this.server = server;
+    }
+
+    @Override
+    public void run()
+    {
+        server.stop(1);
+    }
+}
