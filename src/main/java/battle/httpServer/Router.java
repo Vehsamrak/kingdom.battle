f9f8a2aa2fb2ac50d1f1db11bc29c09777135571
Petr Karmashev <smonkl@bk.ru>
2016-08-02 22:36:40 +0300

    Routing controllers in Router class

--- src/main/java/battle/httpServer/Router.java
+++ src/main/java/battle/httpServer/Router.java
@@ -1,10 +1,16 @@
 package battle.httpServer;
 
+import battle.httpServer.error.AbstractHttpServerError;
+import battle.httpServer.error.RouteNotFound;
+import battle.httpServer.controller.AbstractController;
+import battle.httpServer.controller.DefaultController;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
 
 /**
  * @author Vehsamrak
@@ -12,21 +18,59 @@ import java.io.OutputStream;
 public class Router implements HttpHandler
 {
 
-    public static final int HTTP_OK = 200;
-
     @Override
     public void handle(HttpExchange exchange) throws IOException
     {
-        StringBuilder stringBuilder = new StringBuilder();
+        String route = exchange.getRequestURI().getPath();
 
-        stringBuilder.append("test output");
+        if (this.isRouteExists(route)) {
+            AbstractController controller = this.getControllerByRoute(route);
+            String html = controller.render();
 
-        byte[] bytes = stringBuilder.toString().getBytes();
+            this.sendResponse(exchange, HttpCode.OK, html);
+        } else {
+            this.sendResponse(exchange, new RouteNotFound());
+        }
+    }
 
-        exchange.sendResponseHeaders(HTTP_OK, bytes.length);
+    private void setContentTypeHeader(HttpExchange exchange)
+    {
+        Headers headers = exchange.getResponseHeaders();
+        headers.add("Content-Type", "application/json");
+    }
+
+    public void sendResponse(HttpExchange exchange, HttpCode httpCode, String html) throws IOException
+    {
+        byte[] bytes = html.getBytes();
+
+        this.setContentTypeHeader(exchange);
+        exchange.sendResponseHeaders(httpCode.getCode(), bytes.length);
 
         OutputStream output = exchange.getResponseBody();
         output.write(bytes);
         output.close();
+    }
+    public void sendResponse(HttpExchange exchange, AbstractHttpServerError error) throws IOException
+    {
+        this.sendResponse(exchange, error.getHttpCode(), error.getMessage());
+    }
+
+    private AbstractController getControllerByRoute(String route)
+    {
+        return this.getRoutes().get(route);
+    }
+
+    private boolean isRouteExists(String route)
+    {
+        return this.getRoutes().containsKey(route);
+    }
+
+    private HashMap<String, AbstractController> getRoutes()
+    {
+        HashMap<String, AbstractController> routes = new HashMap<String, AbstractController>();
+
+        routes.put("/", new DefaultController());
+
+        return routes;
     }
 }
